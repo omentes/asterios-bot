5eedec126f55cd99e09f72b0878c54804e82beac
Ubuntu <ubuntu@ip-172-31-40-197.eu-central-1.compute.internal>
2020-01-20 14:39:31 +0000

    update db.sql, create .env.dist

--- db.sql
+++ db.sql
@@ -1,25 +1,50 @@
+-- MySQL dump 10.13  Distrib 5.7.28, for Linux (x86_64)
+--
+-- Host: localhost    Database: asterios
+-- ------------------------------------------------------
+-- Server version	5.7.28-0ubuntu0.18.04.4
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `new_raids`
+--
+
 DROP TABLE IF EXISTS `new_raids`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `new_raids` (
-                             `id` int(11) NOT NULL AUTO_INCREMENT,
-                             `server` int(11) NOT NULL,
-                             `title` varchar(200) NOT NULL,
-                             `description` varchar(200) DEFAULT NULL,
-                             `timestamp` varchar(20) NOT NULL,
-                             `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
-                             PRIMARY KEY (`id`),
-                             UNIQUE KEY `new_raids_server_title_timestamp_uindex` (`server`,`title`,`timestamp`),
-                             KEY `new_raids_created_at_index` (`created_at`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `server` int(11) NOT NULL,
+  `title` varchar(200) NOT NULL,
+  `description` varchar(200) DEFAULT NULL,
+  `timestamp` varchar(20) NOT NULL,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  `alarm` int(11) DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `new_raids_server_title_timestamp_uindex` (`server`,`title`,`timestamp`),
+  KEY `new_raids_created_at_index` (`created_at`)
+) ENGINE=InnoDB AUTO_INCREMENT=1254 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
-CREATE TABLE `respawn_notification` (
-	`id` INT NOT NULL AUTO_INCREMENT,
-	`server` INT NOT NULL,
-	`title` VARCHAR(200) NOT NULL,
-	`created_at` DATETIME NOT NULL DEFAULT NOW(),
-	`shout` INT DEFAULT '1',
-	KEY `respawn_notification_created_at_index` (`created_at`) USING BTREE,
-    KEY `respawn_notification_shout_index` (`shout`) USING BTREE,
-	PRIMARY KEY (`id`)
-);
+
+--
+-- Dumping data for table `new_raids`
+--
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-01-20 14:37:20
